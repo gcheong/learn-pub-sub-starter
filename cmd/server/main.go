105fefd2bb0c0d1b8d2f243fdb89e08bff77d8c4
Greg Cheong <greg@gregcheong.com>
2026-08-12 11:18:36 -0700

    connect to Peril server and wait for interrupt

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+    amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	connectionStr := "amqp://guest:guest@localhost:5672/"
+	connection, error := amqp.Dial(connectionStr)
+
+	if error != nil {
+		log.Fatalf("could not connect to RabbitMQ: %v", error)
+	}
+
+	defer connection.Close()
+
+	fmt.Println("Peril game server connected to RabbitMQ!")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+
+	fmt.Println("RabbitMQ connection closed.")
+
 }
